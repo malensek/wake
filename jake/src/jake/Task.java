98511f2ea521f6339202d339c91afffe833f6157
Matthew Malensek <matt@malensek.net>
2015-05-25 23:50:57 -0600

    ctime methods

--- jake/src/jake/Task.java
+++ jake/src/jake/Task.java
@@ -17,5 +17,46 @@ public class Task {
 
     public boolean needsExecution() {
         return true;
+    private long newestChange(List<WakeFile> files) {
+        long newest = Long.MIN_VALUE;
+
+        for (WakeFile file : files) {
+            long change = changeTime(file);
+            if (change < 0) {
+                change = Long.MAX_VALUE;
+            }
+
+            if (change > newest) {
+                newest = change;
+            }
+        }
+
+        return newest;
+    }
+
+    private long oldestChange(List<WakeFile> files) {
+        long oldest = Long.MAX_VALUE;
+
+        for (WakeFile file : files) {
+            long change = changeTime(file);
+            if (change < oldest) {
+                oldest = change;
+            }
+        }
+
+        return oldest;
+    }
+
+    private long changeTime(WakeFile file) {
+        FileTime ft;
+
+        try {
+            ft = (FileTime) Files.getAttribute(file.toPath(), "unix:ctime");
+        } catch (IOException e) {
+            return -1;
+        }
+
+        return ft.toMillis();
+    }
     }
 }
