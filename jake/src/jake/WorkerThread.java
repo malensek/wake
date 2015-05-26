710e4c7ddceebfb44f3a098addb379da660fa7ff
Matthew Malensek <matt@malensek.net>
2015-05-25 23:50:15 -0600

    Instantiate plugins for each worker thread

--- jake/src/jake/WorkerThread.java
+++ jake/src/jake/WorkerThread.java
@@ -1,13 +1,21 @@
 package jake;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinWorkerThread;
 
 public class WorkerThread extends ForkJoinWorkerThread {
 
+    private List<Plugin> plugins;
+
     public WorkerThread(ForkJoinPool pool) {
         super(pool);
-        System.out.println("custom");
+        plugins = new ArrayList<>();
+        plugins.add(new Markdown());
     }
 
+    public List<Plugin> getPlugins() {
+        return this.plugins;
+    }
 }
