84ace53879fe911662583968a83a8bdd8cfe1218
Matthew Malensek <matt@malensek.net>
2015-05-27 16:04:15 -0600

    Add simple "Dashed Title" TitleMaker
    Creates a title like: “Website - Page”

---
+++ src/jake/core/DashedTitle.java
@@ -0,0 +1,31 @@
+package jake.core;
+
+import org.apache.velocity.VelocityContext;
+
+public class DashedTitle implements TitleMaker {
+
+    private static final String titleKey = "title";
+
+    private String baseTitle;
+    private String separator = " - ";
+
+    public DashedTitle(String baseTitle) {
+        this.baseTitle = baseTitle;
+    }
+
+    public DashedTitle(String baseTitle, String separator) {
+        this.baseTitle = baseTitle;
+        this.separator = separator;
+    }
+
+    public void makeTitle(VelocityContext context, WakeFile file) {
+        String subTitle = "";
+        if (context.containsKey(titleKey)) {
+            subTitle += separator;
+            subTitle += context.get(titleKey);
+        }
+
+        context.put("title", baseTitle + subTitle);
+    }
+
+}
