9f4d42ef7d670a486fd46e90852b3f4a8d324e16
Matthew Malensek <matt@malensek.net>
2015-05-27 16:05:56 -0600

    Implement Copy plugin

---
+++ src/jake/plugins/Copy.java
@@ -0,0 +1,53 @@
+package jake.plugins;
+
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import jake.core.Plugin;
+import jake.core.WakeFile;
+
+public class Copy implements Plugin {
+
+    /* File copy has no dependencies: */
+    private List<WakeFile> dependencies = new ArrayList<>();
+
+    public Copy() {
+
+    }
+
+    @Override
+    public String name() {
+        return "Copy";
+    }
+
+    @Override
+    public boolean wants(WakeFile file) {
+        /* We'll copy anything we can get our hands on! */
+        return true;
+    }
+
+    @Override
+    public List<WakeFile> requires(WakeFile file) {
+        return this.dependencies;
+    }
+
+    @Override
+    public List<WakeFile> produces(WakeFile file) {
+        WakeFile output = file.getOutputFile();
+        ArrayList<WakeFile> outputs = new ArrayList<>();
+        outputs.add(output);
+        return outputs;
+    }
+
+    @Override
+    public void process(WakeFile file) throws Exception {
+        WakeFile output = file.getOutputFile();
+        output.mkParentDir();
+        Files.copy(file.toPath(), output.toPath());
+        System.out.println(this.name() + ": "
+                + file.getRelativePath() + " -> "
+                + output.getRelativePath());
+    }
+
+}
