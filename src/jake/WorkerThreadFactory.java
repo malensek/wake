cc5df9bbfe6fb7da22fe6266c2536d473ef61f2e
Matthew Malensek <matt@malensek.net>
2015-05-26 23:12:08 -0600

    Reformat for 80 cols

--- src/jake/WorkerThreadFactory.java
+++ src/jake/WorkerThreadFactory.java
@@ -3,7 +3,8 @@ package jake;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinWorkerThread;
 
-public class WorkerThreadFactory implements ForkJoinPool.ForkJoinWorkerThreadFactory {
+public class WorkerThreadFactory
+    implements ForkJoinPool.ForkJoinWorkerThreadFactory {
 
     public ForkJoinWorkerThread newThread(ForkJoinPool pool) {
         return new WorkerThread(pool);
