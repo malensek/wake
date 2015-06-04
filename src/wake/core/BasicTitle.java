1355e6cb95f1249540187ac046ed6eff0aafe8b4
Matthew Malensek <matt@malensek.net>
2015-06-03 23:04:08 -0600

    Add simple passthrough title maker
    BasicTitle simply adds whatever title is defined by the file’s front
    matter to the page.

---
+++ src/wake/core/BasicTitle.java
@@ -0,0 +1,22 @@
+package wake.core;
+
+import org.apache.velocity.VelocityContext;
+
+public class BasicTitle implements TitleMaker {
+
+    private static final String titleKey = "title";
+
+    public BasicTitle() {
+
+    }
+
+    public void makeTitle(VelocityContext context, WakeFile file) {
+        String title = "";
+        if (context.containsKey(titleKey)) {
+            title = (String) context.get(titleKey);
+        }
+
+        context.put("title", title);
+    }
+
+}
