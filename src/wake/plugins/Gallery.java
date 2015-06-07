044eee7de773447d632c5a705ee3c8e64611e2e7
Matthew Malensek <matt@malensek.net>
2015-06-07 16:55:21 -0600

    Initial implementation of wants()

--- src/wake/plugins/Gallery.java
+++ src/wake/plugins/Gallery.java
@@ -1,11 +1,14 @@
 package wake.plugins;
 
+import java.io.File;
 import java.util.List;
 
 import wake.core.Plugin;
 import wake.core.WakeFile;
 
 public class Gallery implements Plugin {
+
+    private static final String galleryFileName = "gallery.json";
 
     @Override
     public String name() {
@@ -14,7 +17,19 @@ public class Gallery implements Plugin {
 
     @Override
     public boolean wants(WakeFile file) {
-        return file.getExtension().equals("md");
+        if (file.getName().equals(galleryFileName)) {
+            return true;
+        }
+
+        File fileDir = file.getParentFile();
+        File galleryFile = new File(
+                fileDir.getAbsolutePath() + "/" + galleryFileName);
+        if (galleryFile.exists()) {
+            /* TODO: is this file an image? */
+
+        }
+
+        return false;
     }
 
     @Override
