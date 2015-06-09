af798699153f530b617d861348741de69ad396f0
Matthew Malensek <matt@malensek.net>
2015-06-08 23:56:14 -0600

    Add the Gallery plugin to master plugin list

--- src/wake/exec/WorkerThread.java
+++ src/wake/exec/WorkerThread.java
@@ -2,6 +2,7 @@ package wake.exec;
 
 import wake.core.Plugin;
 import wake.plugins.Copy;
+import wake.plugins.Gallery;
 import wake.plugins.Markdown;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class WorkerThread extends ForkJoinWorkerThread {
     public WorkerThread(ForkJoinPool pool) {
         super(pool);
         plugins = new ArrayList<>();
+        plugins.add(new Gallery());
         plugins.add(new Markdown());
         plugins.add(new Copy());
     }
