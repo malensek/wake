2c7dd7300fd15c10cbf1fd6ee1f0a3cd44b4a260
Matthew Malensek <matt@malensek.net>
2015-07-16 19:13:58 -0600

    Add methods for image scaling/resizing

--- src/wake/plugins/Gallery.java
+++ src/wake/plugins/Gallery.java
@@ -1,14 +1,22 @@
 package wake.plugins;
 
+import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.activation.MimetypesFileTypeMap;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
 
 import wake.core.Plugin;
 import wake.core.WakeFile;
@@ -64,6 +72,52 @@ public class Gallery implements Plugin {
         List<WakeFile> outputs = new ArrayList<>();
         return outputs;
     }
+    private BufferedImage scaleImage(
+            BufferedImage source, int width, int height) {
+
+        int scaledWidth;
+        int scaledHeight;
+
+        int sw = source.getWidth();
+        int sh = source.getHeight();
+
+        if (width > sw && height > sh) {
+            /* Don't increase the size of images that are too small */
+            return source;
+        }
+
+        if (sw == sh) {
+            /* If dimensions are equal, we don't need to worry about scaling */
+            return resizeImage(source, width, height);
+        }
+
+        if (sw > sh) {
+            scaledWidth = width;
+            scaledHeight = Math.round(sh * ((float) width / (float) sw));
+        } else {
+            scaledHeight = height;
+            scaledWidth = Math.round(sw * ((float) height / (float) sh));
+        }
+
+        return resizeImage(source, scaledWidth, scaledHeight);
+    }
+
+    private BufferedImage resizeImage(
+            BufferedImage source, int width, int height) {
+
+        BufferedImage resizedImg = new BufferedImage(
+                width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = resizedImg.createGraphics();
+        g.setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(source, 0, 0, width, height, null);
+        g.dispose();
+
+        return resizedImg;
+    }
+
     private boolean isGalleryFile(File file) {
         return file.getName().equals(galleryFileName);
     }
