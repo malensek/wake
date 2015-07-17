6c797fea381eb6ae816739112028ffed8f8c9a73
Matthew Malensek <matt@malensek.net>
2015-07-17 16:41:34 -0600

    Remove mimeType method (migrated to util)

--- src/wake/plugins/Gallery.java
+++ src/wake/plugins/Gallery.java
@@ -245,24 +245,4 @@ public class Gallery implements Plugin {
         }
         return null;
     }
-
-    /**
-     * Retrieves the MIME type of the specified file.
-     */
-    private String mimeType(File file) {
-        String mimeType = null;
-        try {
-            mimeType = Files.probeContentType(file.toPath());
-        } catch (Exception e) {
-            /* Exceptions are ignored here; the fallback method will be used
-             * instead. */
-        }
-
-        /* Fallback method */
-        if (mimeType == null) {
-            mimeType = new MimetypesFileTypeMap().getContentType(file);
-        }
-
-        return mimeType;
-    }
 }
