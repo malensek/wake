d73b08151e42cb84ab7c74726e0af422c168cc70
Matthew Malensek <matt@malensek.net>
2016-07-16 00:58:44 -0600

    Add redirect for dirs without trailing slash

--- src/main/java/io/sigpipe/wake/server/RequestHandler.java
+++ src/main/java/io/sigpipe/wake/server/RequestHandler.java
@@ -3,8 +3,11 @@ package io.sigpipe.wake.server;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 import java.nio.file.Files;
+import java.util.Arrays;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -12,8 +15,28 @@ class RequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println("Request: " + exchange.getRequestURI());
-        File localFile = new File("." + exchange.getRequestURI());
+        String method = exchange.getRequestMethod();
+        String uri = exchange.getRequestURI().getPath();
+//        if (method != "GET") {
+//            return;
+//        }
+
+        System.out.println(method + " " + uri);
+
+        File localFile = new File("." + uri);
+        if (localFile.isDirectory() && uri.endsWith("/") == false) {
+            /* Redirect to URL with trailing slash */
+            uri += "/";
+            System.out.println("Redirecting to: " + uri);
+            String message = "Redirected";
+            exchange.getResponseHeaders().put("Location", Arrays.asList(uri));
+            exchange.sendResponseHeaders(301, message.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(message.getBytes());
+            os.close();
+            return;
+        }
+
         if (localFile.isDirectory()) {
             localFile = new File(localFile.getAbsolutePath() + "/index.html");
         }
@@ -25,6 +48,13 @@ class RequestHandler implements HttpHandler {
             OutputStream os = exchange.getResponseBody();
             Files.copy(localFile.toPath(), os);
             os.close();
+        }
+    }
+
+    private void printHeaders(HttpExchange exchange) {
+        Headers headers = exchange.getRequestHeaders();
+        for (String key : headers.keySet()) {
+            System.out.println(key + " -> " + headers.get(key));
         }
     }
 
