3a2aa24f46ee5eb9cff7e327eb80e0ed8e85f48e
Matthew Malensek <matt@malensek.net>
2017-10-05 13:23:27 -0700

    Convert to flexmark

--- src/main/java/io/sigpipe/wake/plugins/gallery/Gallery.java
+++ src/main/java/io/sigpipe/wake/plugins/gallery/Gallery.java
@@ -14,12 +14,23 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.anchorlink.AnchorLinkExtension;
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughSubscriptExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.toc.TocExtension;
+import com.vladsch.flexmark.ext.typographic.TypographicExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.parser.ParserEmulationProfile;
+import com.vladsch.flexmark.util.KeepType;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.pegdown.Extensions;
-import org.pegdown.PegDownProcessor;
 
 import io.sigpipe.wake.core.Configuration;
 import io.sigpipe.wake.core.Plugin;
@@ -41,13 +52,39 @@ public class Gallery implements Plugin {
     protected static final String galleryFileName = "gallery_index.md";
     protected static final String galleryTemplate = "gallery.vm";
 
-    private VelocityEngine velocityEngine;
-    private PegDownProcessor markdownProcessor;
+    private HtmlRenderer htmlRenderer;
+    private Parser markdownParser;
     private WakeFile templateFile;
     private Template template;
 
     public Gallery()
     throws PluginInitializationException {
+        MutableDataSet options = new MutableDataSet();
+        options.setFrom(ParserEmulationProfile.GITHUB_DOC);
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+                AutolinkExtension.create(),
+                AnchorLinkExtension.create(),
+                StrikethroughSubscriptExtension.create(),
+                TablesExtension.create(),
+                TaskListExtension.create(),
+                TocExtension.create(),
+                TypographicExtension.create()
+        ));
+
+        /* GFM table parsing options */
+        options.set(TablesExtension.COLUMN_SPANS, false)
+            .set(TablesExtension.MIN_HEADER_ROWS, 1)
+            .set(TablesExtension.MAX_HEADER_ROWS, 1)
+            .set(TablesExtension.APPEND_MISSING_COLUMNS, true)
+            .set(TablesExtension.DISCARD_EXTRA_COLUMNS, true)
+            .set(TablesExtension.WITH_CAPTION, false)
+            .set(TablesExtension.HEADER_SEPARATOR_COLUMN_MATCH, true);
+
+        options
+            .set(HtmlRenderer.INDENT_SIZE, 4)
+            .set(HtmlRenderer.OBFUSCATE_EMAIL, true)
+            .set(Parser.REFERENCES_KEEP, KeepType.LAST);
+
         Configuration config = Configuration.instance();
 
         templateFile = new WakeFile(config.getTemplateDir(), galleryTemplate);
@@ -55,13 +92,14 @@ public class Gallery implements Plugin {
             throw new PluginInitializationException(
                     "Could not locate gallery template");
         }
-        velocityEngine = new VelocityEngine();
+        VelocityEngine velocityEngine = new VelocityEngine();
         velocityEngine.setProperty("file.resource.loader.path",
                 config.getTemplateDir().getAbsolutePath());
         velocityEngine.init();
         template = velocityEngine.getTemplate(templateFile.getName());
 
-        markdownProcessor = new PegDownProcessor(Extensions.ALL);
+        markdownParser = Parser.builder(options).build();
+        htmlRenderer = HtmlRenderer.builder(options).build();
     }
 
     @Override
@@ -183,7 +221,8 @@ public class Gallery implements Plugin {
         VelocityContext context = new VelocityContext(yamlData);
         Configuration.instance().getTitleMaker().makeTitle(context, file);
 
-        String html = markdownProcessor.markdownToHtml(content);
+        Node document = markdownParser.parse(content);
+        String html = htmlRenderer.render(document);
         context.put("markdown_content", html);
         context.put("thumbnail_content", thumbnailHtml.toString());
 
