44f2559ba601435f23e5fe6cb306dc2424aea71b
Matthew Malensek <matt@malensek.net>
2017-10-05 16:25:34 -0700

    Clean up interfaces

--- src/main/java/io/sigpipe/wake/util/SharedDatasetAccessor.java
+++ src/main/java/io/sigpipe/wake/util/SharedDatasetAccessor.java
@@ -10,12 +10,12 @@ public interface SharedDatasetAccessor {
      * Creates a shared dataset. This operation will be performed by the first
      * thread that tries to access the dataset.
      */
-    public Dataset createDataset();
+    Dataset createDataset();
 
     /**
      * Retrieves this dataset's unique ID. Generally, this is the absolute path
      * name of the file or directory that is associated with the dataset.
      */
-    public String getDatasetID();
+    String getDatasetID();
 
 }
