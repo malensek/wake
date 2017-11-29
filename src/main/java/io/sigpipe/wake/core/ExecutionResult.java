7d7357bc9241e5aac5921a9f9b13303dd45e3a02
Matthew Malensek <matt@malensek.net>
2017-11-28 18:17:43 -0800

    Update package and license

--- src/main/java/io/sigpipe/wake/core/ExecutionResult.java
+++ src/main/java/io/sigpipe/wake/core/ExecutionResult.java
@@ -1,8 +1,29 @@
-/* wake - http://sigpipe.io/wake                       *
- * Copyright (c) 2016 Matthew Malensek                 *
- * Distributed under the MIT License (see LICENSE.txt) */
+/*
+Copyright (c) 2017, Matthew Malensek
+All rights reserved.
 
-package io.sigpipe.wake.exec;
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall the copyright holder or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on
+any theory of liability, whether in contract, strict liability, or tort
+(including negligence or otherwise) arising in any way out of the use of this
+software, even if advised of the possibility of such damage.
+*/
+
+package io.sigpipe.wake.core;
 
 import java.util.List;
 
