07eab76b110b7cee3bedf4b04007a43c918b52ea
Matthew Malensek <matt@malensek.net>
2018-01-03 16:13:27 -0800

    Move default Markdown settings to Configuration

--- src/main/java/io/sigpipe/wake/core/Configuration.java
+++ src/main/java/io/sigpipe/wake/core/Configuration.java
@@ -9,12 +9,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import com.esotericsoftware.yamlbeans.YamlException;
 import com.esotericsoftware.yamlbeans.YamlReader;
+
+import com.vladsch.flexmark.ext.anchorlink.AnchorLinkExtension;
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughSubscriptExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.toc.TocExtension;
+import com.vladsch.flexmark.ext.typographic.TypographicExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.parser.ParserEmulationProfile;
+import com.vladsch.flexmark.util.KeepType;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 
 public class Configuration {
 
@@ -106,6 +120,36 @@ public class Configuration {
 
     public Set<PosixFilePermission> getDirPermissions() {
         return dirPerm;
+    }
+
+    public MutableDataSet getMarkdownOptions() {
+        MutableDataSet options = new MutableDataSet();
+        options.setFrom(ParserEmulationProfile.GITHUB_DOC);
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+                AutolinkExtension.create(),
+                AnchorLinkExtension.create(),
+                StrikethroughSubscriptExtension.create(),
+                TablesExtension.create(),
+                TaskListExtension.create(),
+                TocExtension.create(),
+                TypographicExtension.create()
+        ));
+
+        /* GFM table parsing options */
+        options.set(TablesExtension.COLUMN_SPANS, false)
+            .set(TablesExtension.MIN_HEADER_ROWS, 1)
+            .set(TablesExtension.MAX_HEADER_ROWS, 1)
+            .set(TablesExtension.APPEND_MISSING_COLUMNS, true)
+            .set(TablesExtension.DISCARD_EXTRA_COLUMNS, true)
+            .set(TablesExtension.WITH_CAPTION, false)
+            .set(TablesExtension.HEADER_SEPARATOR_COLUMN_MATCH, true);
+
+        options
+            .set(HtmlRenderer.INDENT_SIZE, 4)
+            .set(HtmlRenderer.OBFUSCATE_EMAIL, true)
+            .set(Parser.REFERENCES_KEEP, KeepType.LAST);
+
+        return options;
     }
 
     private File readDirConfig(String defaultName, Map<?, ?> config) {
