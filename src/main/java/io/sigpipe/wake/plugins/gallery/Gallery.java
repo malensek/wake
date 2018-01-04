7d44b622c0b9200f5e8d0bb009f88ad817aa542d
Matthew Malensek <matt@malensek.net>
2018-01-03 16:17:18 -0800

    Use new md config option

--- src/main/java/io/sigpipe/wake/plugins/gallery/Gallery.java
+++ src/main/java/io/sigpipe/wake/plugins/gallery/Gallery.java
@@ -36,17 +36,8 @@ import java.util.List;
 import java.util.Map;
 
 import com.vladsch.flexmark.ast.Node;
-import com.vladsch.flexmark.ext.anchorlink.AnchorLinkExtension;
-import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughSubscriptExtension;
-import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
-import com.vladsch.flexmark.ext.tables.TablesExtension;
-import com.vladsch.flexmark.ext.toc.TocExtension;
-import com.vladsch.flexmark.ext.typographic.TypographicExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
-import com.vladsch.flexmark.parser.ParserEmulationProfile;
-import com.vladsch.flexmark.util.KeepType;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 
 import org.apache.velocity.Template;
@@ -79,33 +70,8 @@ public class Gallery implements Plugin {
 
     public Gallery()
     throws PluginInitializationException {
-        MutableDataSet options = new MutableDataSet();
-        options.setFrom(ParserEmulationProfile.GITHUB_DOC);
-        options.set(Parser.EXTENSIONS, Arrays.asList(
-                AutolinkExtension.create(),
-                AnchorLinkExtension.create(),
-                StrikethroughSubscriptExtension.create(),
-                TablesExtension.create(),
-                TaskListExtension.create(),
-                TocExtension.create(),
-                TypographicExtension.create()
-        ));
-
-        /* GFM table parsing options */
-        options.set(TablesExtension.COLUMN_SPANS, false)
-            .set(TablesExtension.MIN_HEADER_ROWS, 1)
-            .set(TablesExtension.MAX_HEADER_ROWS, 1)
-            .set(TablesExtension.APPEND_MISSING_COLUMNS, true)
-            .set(TablesExtension.DISCARD_EXTRA_COLUMNS, true)
-            .set(TablesExtension.WITH_CAPTION, false)
-            .set(TablesExtension.HEADER_SEPARATOR_COLUMN_MATCH, true);
-
-        options
-            .set(HtmlRenderer.INDENT_SIZE, 4)
-            .set(HtmlRenderer.OBFUSCATE_EMAIL, true)
-            .set(Parser.REFERENCES_KEEP, KeepType.LAST);
-
         Configuration config = Configuration.instance();
+        MutableDataSet options = config.getMarkdownOptions();
 
         templateFile = new WakeFile(config.getTemplateDir(), galleryTemplate);
         if (templateFile.exists() == false) {
@@ -153,10 +119,9 @@ public class Gallery implements Plugin {
             dependencies.add(file.getParentFile());
             dependencies.add(this.templateFile);
 
-            Configuration config = Configuration.instance();
             dependencies.addAll(
                 TemplateUtils.getTemplateDependencies(
-                    config, this.templateFile));
+                    Configuration.instance(), this.templateFile));
         } else {
             dependencies.add(galleryFile(file));
         }
